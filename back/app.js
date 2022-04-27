58625e7212b28e7ef7be75937feb5ba7f404a0ca
habibi <jnh03336@gmail.com>
2022-04-27 17:29:11 +0900

    Update: basic operation of playlist read and create routes

--- back/app.js
+++ back/app.js
@@ -1,25 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 3200
+const express = require('express');
+//file system 관련 documentation: https://nodejs.org/api/fs.html
+const { readFile, writeFile } = require('fs/promises');
+// id generating library
+const shortid = require('shortid');
+const app = express();
+const port = 3200;
+const path = './data/'
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/playlists', (req, res) => {
-  // read data and response
+//플레이리스트 정보 READ요청 들어오는 경로
+app.get('/playlists', async (req, res) => {
+  // 한 번 요청마다 적절한 개수의 playlist dat를 response
   res.send('data');
 })
 
-app.get('/playlists/:playlistId', (req, res) => {
+//특정 플레이리스트 정보 READ요청 들어오는 경로
+app.get('/playlists/:playlistId', async (req, res) => {
+  // playlistId에 해당하는 파일을 읽어서 관련 정보를 return
   const playlistId = req.params.playlistId;
-  // read data of given playlist id and response
-  res.send('data');
+  try {
+    let playlist = await readFile(path + 'playlists/' + playlistId, 'utf8');
+    res.send(playlist);
+  } catch {
+    res.send('error!');
+  }
 })
 
-app.post('/playlists', (req, res) => {
-  // data processing and store
-  res.send('success')
+//레이리스트 CREATE 요청 들어오는 경로
+app.post('/playlists', async (req, res) => {
+  try {
+    // * req로 받은 data를 적절히 변형하는 과정 필요
+    let data = {};
+    await writeFile(path + 'playlists/' + shortid.generate(), JSON.stringify(data));
+    res.send('create success');
+  } catch (error) {
+    console.log(error);
+    res.send('error!');
+  }
 })
 
 app.listen(port, () => {
