27cdc46729db5f97653e13997b0cc1c648b4f364
habibi <jnh03336@gmail.com>
2022-04-27 16:28:16 +0900

    ADD: construct basic routes

--- back/app.js
+++ back/app.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/playlists', (req, res) => {
+  // read data and response
+  res.send('data');
+})
+
+app.get('/playlists/:playlistId', (req, res) => {
+  const playlistId = req.params.playlistId;
+  // read data of given playlist id and response
+  res.send('data');
+})
+
+app.post('/playlists', (req, res) => {
+  // data processing and store
+  res.send('success')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
