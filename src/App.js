24d00e6e4e0556f4dcde00c907346aac00e87747
표정은 <sharron315@naver.com>
2022-04-29 06:43:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App=()=>
 
   return(
     <div>
-        {"<IntroComponent></IntroComponent>"}
+        <IntroComponent></IntroComponent>
         <MainComponent></MainComponent>
        
     </div>
